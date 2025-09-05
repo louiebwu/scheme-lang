81c42d6ac68b2cc02da76454d125db7ca242a7a7
louiebwu <lwu2012@hotmail.com>
2025-09-05 15:24:34 +0100

    Completed parser combinator logic

--- parser.hs
+++ parser.hs
@@ -26,3 +26,55 @@ Tok.commentStart = "{-"
 }
 
 Tok.TokenParser { Tok.parens = m_parens, Tok.identifier = m_identifier } = Tok.makeTokenParser style
+
+reservedOp :: T.Text -> Parser ()
+reservedOp op = Tok.reservedOp lexer $ T.unpack op
+
+-- Parser
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    p <- m_identifier
+    return $ Atom $ T.pack p
+
+parseText :: Parser LispVal
+parseText = do
+    reservedOp "\""
+    p <- many1 $ noneOf "\""
+    reservedOp "\""
+    return $ String . T.pack $ p
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+
+parseNegNum :: Parser LispVal
+parseNegNum = do
+    char '-'
+    d <- many1 digit
+    return $ Number . negate . read $ d
+
+parseList :: Parser LispVal
+parseList = List . concat <$> Text.Parsec.many parseExpr `sepBy` (char ' ' <|> char '\n')
+
+parseSExp = List . concat <$> m_parens (Text.Parsec.many parseExpr `sepBy` (char ' ' <|> char '\n'))
+
+parseQuote :: Parser LispVal
+parseQuote = do
+    reservedOp "\<"
+    x <- parseExpr
+    return $ List [Atom "quote", x]
+
+parseReserved :: Parser LispVal
+parseReserved = do
+    reservedOp "Nil" >> return Nil
+    <|> (reservedOp "#t" >> return (Bool True))
+    <|> (reservedOp "#f" >> return (Bool False))
+
+parseExpr :: Parser LispVal
+parseExpr = parseReserved 
+    <|> parseNumber
+    <|> try parseNegNum
+    <|> parseAtom
+    <|> parseText
+    <|> parseQuote
+    <|> parseSExp
